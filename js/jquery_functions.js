77ad8628e01d673c5d16aecc499b94bce1133524
iris-su <iris.anna.su@gmail.com>
2020-11-22 17:38:24 -0500

    hook and context

--- js/jquery_functions.js
+++ js/jquery_functions.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     /* Hook Visualization Functions */
 
     // Hiding later information!
-    $('#comparingGuess').hide();
+    $('#hook-comparingGuess').hide();
 
     // Making sure that the user inputs a valid number
     $('#userSquirNum').keyup(function(){
@@ -18,12 +18,38 @@ $(document).ready(function(){
     });
 
     // After the user submits, hide the guessing and cue the information!
-    $('#askingUserSquirNum').submit(function(){
-        $('#userGuess').hide(1000);
-        $('#comparingGuess').show(1000);
+    $('#squirrelForm').submit(function(){
+        var userInput = $('#userSquirNum').val();
+        var totalSquirrels = 2373
+        var userOff = Math.abs(userInput - totalSquirrels).toLocaleString();
+
+        $('#userGuess').fadeOut(800);
+        $('#hook-comparingGuess').delay(1000).fadeIn(1600);
+        $('#comparingSquirNum').html("You were off by " + userOff + " squirrels. There are actually <strong> 2,373 unique squirrels </strong> in Central Park!");
+
     });
 
     /* Context Visualization Functions */
+
+    // Hiding everything that we need to
+    $('#dataContext').hide();
+    $('#historicalContext').fadeTo(0, 0);
+    $('#nextButtonHistorical').hide();
+
+    // Giving more information if prompted
+    $('#transitionButton').click(function(){
+        $('#transitionButton').fadeOut(800);
+        $('#transition2Context').fadeOut(800);
+        $('#dataContext').delay(1000).fadeIn(1600);
+        $('#nextButtonHistorical').delay(1000).fadeIn(1600);
+    })
+
+    // Providing more historical information
+    $('#nextButtonHistorical').click(function(){
+        $('#nextButtonHistorical').fadeOut(100);
+        $('#dataContext').fadeTo(1600, 0);
+        $('#historicalContext').fadeTo(1600, 1);
+    })
 
     /* Squirrel Trivia Visualization Functions */
 

9b1e84cade5c4840983430a1f3e7485a5df60ea5
iris-su <iris.anna.su@gmail.com>
2020-12-06 13:17:41 -0500

    added faces, trivia, homepage

--- js/jquery_functions.js
+++ js/jquery_functions.js
@@ -4,6 +4,32 @@
 
 $(document).ready(function(){
 
+    /* Homepage Animations */
+    $('#titleSquirrel').fadeTo(0,0)
+    $('#titleCity').fadeTo(0,0)
+    $('#bench').hide()
+    $('#tree1').hide()
+    $('#tree2').hide()
+    $('#tree3').hide()
+    $('#tree4').hide()
+    $('#tree5').hide()
+    $('#tree6').hide()
+    $('#tree7').hide()
+    $('#homepagesquirrel').hide()
+
+    $('#titleSquirrel').delay(800).fadeTo(1200, 1);
+    $('#titleCity').delay(1600).fadeTo(1200, 1);
+    $('#bench').delay(1800).fadeIn(1200);
+    $('#tree5').delay(2000).fadeIn(1200);
+    $('#tree4').delay(2200).fadeIn(1200);
+    $('#tree3').delay(2400).fadeIn(1200);
+    $('#tree2').delay(2600).fadeIn(1200);
+    $('#tree1').delay(2800).fadeIn(1200);
+    $('#tree6').delay(3000).fadeIn(1200);
+    $('#tree7').delay(3200).fadeIn(1200);
+    $('#homepagesquirrel').delay(4000).fadeIn(1200);
+
+
     /* Hook Visualization Functions */
 
     // Hiding later information!
@@ -45,18 +71,41 @@ $(document).ready(function(){
 
     /* Squirrel Trivia Visualization Functions */
 
-    $('.answer-reveal').hide();
+    // Hiding all answers and question set 2
+    $('.answer-reveal').fadeTo(0, 0);
+    $('.question-set-2').hide();
 
-    $('#question-1-answer').click(function(){
-        $('#answer-1').fadeIn(800);
-    })
+    // Revealing answer to question 1
+    $('#question-1-answers-A').click(function(){
+        $('#answer-1').fadeTo(800, 1);
+    });
 
-    $('#question-2-answer').click(function(){
-        $('#answer-2').fadeIn(800);
-    })
+    $('#question-1-answers-B').click(function(){
+        $('#answer-1').fadeTo(800, 1);
+    });
 
-    $('#question-3-answer').click(function(){
-        $('#answer-3').fadeIn(800);
+    // Revealing answer to question 2
+    $('#question-2-answers-A').click(function(){
+        $('#answer-2').fadeTo(800, 1);
+    });
+
+    $('#question-2-answers-B').click(function(){
+        $('#answer-2').fadeTo(800, 1);
+    });
+
+    // Revealing answer to question 3
+    $('#question-3-answers-A').click(function(){
+        $('#answer-3').fadeTo(800, 1);
+    });
+
+    $('#question-3-answers-B').click(function(){
+        $('#answer-3').fadeTo(800, 1);
+    });
+
+    // More questions (set 2 of questions)
+    $('#moreQuestions').click(function(){
+        $('.question-set-1').hide();
+        $('.question-set-2').hide();
     })
 
     /* Quotes Functions (same as Context Functions) */
